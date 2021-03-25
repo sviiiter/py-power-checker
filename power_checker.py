844acb755caf0e7cb96bfe6f1e6851f908ad9273
sviter <nick>
2021-03-25 23:47:26 +0300

    Send notification only once

--- power_checker.py
+++ power_checker.py
@@ -5,9 +5,29 @@ from notify import Notification
 import json
 from datetime import datetime
 import sys
+import os
 
 # off ssl warning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+def get_data_file(work_type):
+    f_name = 'id_{}.data'.format(work_type)
+    if os.path.isfile(f_name):
+        f = open(f_name, 'r')
+    else:
+        f = open(f_name, 'w+')
+
+    data = f.read()
+    f.close()
+
+    return data
+
+
+def set_data_file(work_type, data):
+    f = open('id_{}.data'.format(work_type), 'w')
+    f.write(data)
+    f.close()
 
 
 for key, value in requestList.items():
@@ -18,6 +38,11 @@ for key, value in requestList.items():
     if returnText != 0:
         try:
             if isinstance(returnText, list):
+                if get_data_file(key) == responseObj.text:
+                    continue
+
+                set_data_file(key, responseObj.text)
+
                 place = returnText[0]['Place']
                 timeFrom = datetime.strptime(returnText[0]['DisconnectionDateTime'], '%Y-%m-%dT%H:%M:%S').strftime('%d.%m.%Y %H:%M')
                 timeTo = datetime.strptime(returnText[0]['EnergyOnPlanningDateTime'], '%Y-%m-%dT%H:%M:%S').strftime('%d.%m.%Y %H:%M')
